3608076a8dd9d30290390f163b95cf15ccd4c4bb
lucas.souza <lucas.souza@fretebras.com.br>
2023-06-11 20:28:29 -0300

    feat: create project

---
+++ src/test/groovy/app/MotoristaSpec.groovy
@@ -0,0 +1,18 @@
+package app
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class MotoristaSpec extends Specification implements DomainUnitTest<Motorista> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
