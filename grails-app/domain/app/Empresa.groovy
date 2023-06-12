0ce16a624085c78f824a9ff6f4372be99e0450ca
lucas.souza <lucas.souza@fretebras.com.br>
2023-06-11 23:43:16 -0300

    feat: add necessary cruds

--- grails-app/domain/app/Empresa.groovy
+++ grails-app/domain/app/Empresa.groovy
@@ -1,7 +1,21 @@
 package app
 
 class Empresa {
+    String nome
+    String cnpj
+    String endereco
+    String telefone
+    String email
+    String responsavel
+    String tipoTransportadora
 
     static constraints = {
+        nome(nullable: false)
+        cnpj(nullable: false, unique: true)
+        endereco(nullable: false)
+        telefone(nullable: false)
+        email(nullable: false, email: true)
+        responsavel(nullable: false)
+        tipo(nullable: false, inList: ["filial", "matriz"])
     }
 }
--- grails-app/domain/app/Usuario.groovy
+++ grails-app/domain/app/Usuario.groovy
@@ -1,7 +1,15 @@
 package app
 
 class Usuario {
-
+    String nome
+    String email
+    String senha
+    Date dataCadastro
+    
     static constraints = {
+        nome(nullable: false)
+        email(nullable: false, email: true, unique: true)
+        senha(nullable: false, maxSize: 100)
+        dataCadastro(nullable: false)
     }
 }
